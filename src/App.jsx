14d4a2b4f8f671875c1bb620900334197fe1d3e4
soyeon1351 <pack6411351@gmail.com>
2025-04-26 04:22:07 +0900

    ✨ Id 1번 데이터 생성
    id 1번의 이름, 설명, 앞과 뒤 이미지에 대한 객체 생성

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,16 @@ function App() {
     const fetchAPI = async () => {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon-species/1/")
       const data = await response.json()
-      console.log(data)
+      console.log(data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text)
+      
+      const pokemonData = {
+        id : 1,
+        name : data.names.find(el => el.language.name === 'ko').name,
+        description : data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text,
+        front : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+        back : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
+      }
+      console.log(pokemonData)
     }
     fetchAPI()
   }, [])
