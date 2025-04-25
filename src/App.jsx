09ee02ecce7020f16f20c16939b01ca6109143a4
soyeon1351 <pack6411351@gmail.com>
2025-04-26 05:19:11 +0900

    ♻️  useEffect를 createAsyncThunk로 리팩토링
     데이터 패칭 로직을 useEffect에서 createAsyncThunk로 변경하여 비동기 흐름 관리 개선

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,7 @@ function App() {
 
   useEffect(()=>{
 
-    const numberArray = Array.from({length: 151},(_, i) => i + 1)
-
-
-    const fetchAPI = async (pokemonId) => {
-
-      // Promise.all([1,2,3, ...,151])
-      // => [이상해씨, 이상해풀, ...뮤]
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`)
-      const data = await response.json()
-      
-      const pokemonData = {
-        id : pokemonId,
-        name : data.names.find(el => el.language.name === 'ko').name,
-        description : data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text,
-        front : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`,
-        back : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemonId}.png`,
-      }
-      return pokemonData
-    }
-    const fetchPokemonDatas = async () => {
-      const pokemonDatas = await Promise.all(numberArray.map((el) =>  fetchAPI(el)))
-      console.log(pokemonDatas)
-    }
-    fetchPokemonDatas()
+    
   }, [])
 
   return (
---
+++ src/RTK/thunk.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+createAsyncThunk(
+    'pokemon/fetchMultiplePokemonById',
+    async (maxPokemonId) => {
+        const numberArray = Array.from({length: maxPokemonId},(_, i) => i + 1)
+
+
+        const fetchAPI = async (pokemonId) => {
+    
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`)
+          const data = await response.json()
+          
+          const pokemonData = {
+            id : pokemonId,
+            name : data.names.find(el => el.language.name === 'ko').name,
+            description : data.flavor_text_entries.find(el => el.language.name === 'ko').flavor_text,
+            front : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`,
+            back : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemonId}.png`,
+          }
+          return pokemonData
+        }
+        const fetchPokemonDatas = async () => {
+          const pokemonDatas = await Promise.all(numberArray.map((el) =>  fetchAPI(el)))
+          return (pokemonDatas)
+        }
+        return fetchPokemonDatas()
+    }
+)
