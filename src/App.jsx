2cd800bc9fe124c5ef188f836948bdfa0ab45978
soyeon1351 <pack6411351@gmail.com>
2025-04-30 03:43:21 +0900

    ✨ ✨ 라우팅 설정 및 페이지 링크 연결
    ✨ React Router를 사용하여 Routes, Route, Link를 설정하고 각 페이지 간 이동 가능하도록 구성

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useEffect } from 'react'
 import './App.scss'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchMultiplePokemonById } from './RTK/thunk'
+import { Link, Routes, Route } from 'react-router-dom'
+import Favorite from './pages/Favorite'
+import Search from './pages/Search'
+import Main from './pages/Main'
+import Detail from './pages/Detail'
 
 function App() {
   const dispatch = useDispatch()
@@ -14,6 +19,18 @@ function App() {
   return (
     <>
     <h1 className='text-[40px] text-center'>포켓몬 도감</h1>
+    <nav>
+      <Link to={'/'}>메인</Link>
+      <Link to={'/detail/pokemon'}>상세정보</Link>
+      <Link to={'/Search'}>검색</Link>
+      <Link to={'/Favorite'}>찜목록</Link>
+    </nav>
+    <Routes>
+      <Route path={'/'} element={<Main />}></Route>
+      <Route path={'/detail/pokemon'} element={<Detail />}></Route>
+      <Route path={'/Search'} element={<Search />}></Route>
+      <Route path={'/Favorite'} element={<Favorite />}></Route>
+    </Routes>
     </>
   
   )
