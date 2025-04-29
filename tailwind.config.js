3ff5845a07fe6d322731eeca0a36c35f69f7ef78
soyeon1351 <pack6411351@gmail.com>
2025-04-30 03:25:28 +0900

    ♻️ 스타일링 환경 기본셋팅 수정
    기본 css 셋팅 삭제 후 scss로 변경, tailwind파일 내용 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.hrml",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
